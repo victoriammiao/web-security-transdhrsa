0c4a951dc21c03bbe0b38e80ff1b97305368dd83
victoriammiao <2676129376@qq.com>
2025-10-16 20:59:15 +0800

    下拉列表和输入收件人完成

--- transportationDHRSA/client/src/components/RegisterForm.vue
+++ transportationDHRSA/client/src/components/RegisterForm.vue
@@ -28,7 +28,7 @@ const register = async () => {
   }
 
   try {
-    const res = await fetch("http://localhost:3000/api/register", {
+    const res = await fetch("http://localhost:3000/api/auth/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
